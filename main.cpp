0abc26a50ef678489d08ed8040c5247bed1a7abc
Pradeep <pradeepmuralidhar29@gmail.com>
2024-09-03 13:07:21 +0530

    first c++ code

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Pradeep";
+    int age = 23;
+    cout << "hello people iam " << name << " and iam this year old " << age << endl;
+}
